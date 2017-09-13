dab5f4b256daae15f38638d6a578ee0892c7eb38
Alejandro Peinó <alepeino@gmail.com>
2017-09-12 21:50:16 -0300

    Sidebar

--- src/cljs/money_1846/views.cljs
+++ src/cljs/money_1846/views.cljs
@@ -1,7 +1,22 @@
 (ns money-1846.views
-  (:require [re-frame.core :as re-frame]))
+  (:require
+    [reagent.core :as r]
+    [re-frame.core :as re-frame]))
+
+(defn sidebar []
+  (let [open (r/atom false)]
+    (fn []
+      [:div#sidebar
+       [:nav.p-1.bg-danger
+        [:button.navbar-toggler.align-self-start
+          {:type "button"
+           :onClick #(swap! open not)}
+          [:span.navbar-toggler-icon.small]]]
+       [:div.sidebar-content.w-50.bg-info {:class (when @open "open")}]])))
 
 (defn main []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello from " @name])))
+      [:div
+       [sidebar]
+       [:div.content "Hello from " @name]])))
