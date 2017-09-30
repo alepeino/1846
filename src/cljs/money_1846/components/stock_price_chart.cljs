2b8f43b8b359e987d572f4dfb6abc89e0d131051
Alejandro Peinó <alepeino@gmail.com>
2017-09-29 23:29:12 -0300

    Reduce stock chart height

--- src/cljs/money_1846/components/stock_price_chart.cljs
+++ src/cljs/money_1846/components/stock_price_chart.cljs
@@ -6,6 +6,14 @@
 
 (def dragula (.-dragula js/window))
 
+(def token-margins {1 0
+                    2 3
+                    3 1.4
+                    4 0.1
+                    5 -0.6
+                    6 -1.1
+                    7 -1.4})
+
 (defn corporation-stock-token [props id]
   [:div {:data-corporation-id id}
    [:img.rounded-circle.w-100.border.border-primary
@@ -20,7 +28,7 @@
    (for [{id :id} (sort-by :price-last-updated corporations)]
      ^{:key id}
      [corporation-stock-token
-      {:style {:margin-bottom (-> 3.2 (- (* 0.6 (count corporations))) (str "vw"))}}
+      {:style {:margin-bottom (-> corporations count token-margins (str "vw"))}}
       id])])
 
 (defn stock-price-chart [corporations chart-values]
@@ -39,7 +47,7 @@
                                                               (-> segment .-dataset .-stockValue js/parseInt)])))))
         ref #(when % (-> % (.-children) (js/Array.from) (dragula dragula-options) ((partial reset! drake)) (dragula-events)))]
     [:div.d-flex.w-100 {:ref ref
-                        :style {:height (str (* 5 segment-width) "vw")}}
+                        :style {:height (str (* 4 segment-width) "vw")}}
      (for [value chart-values]
        ^{:key value}
        [stock-price-chart-segment
