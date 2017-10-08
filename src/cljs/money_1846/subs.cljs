b4d9c4fa90e30a73932419a1b40a41c7a3199b19
Alejandro Peinó <alepeino@gmail.com>
2017-10-08 12:47:29 -0300

    Refact. simple subs

--- src/cljs/money_1846/subs.cljs
+++ src/cljs/money_1846/subs.cljs
@@ -2,18 +2,12 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :as rf]))
 
-(rf/reg-sub
- :corporations
- (fn [db]
-   (:corporations db)))
+(rf/reg-sub :players :players)
+
+(rf/reg-sub :corporations :corporations)
 
 (rf/reg-sub
   :in-game-corporations
   :<- [:corporations]
   (fn [corporations]
     (filter (comp :in-game second) corporations)))
-
-(rf/reg-sub
-  :players
-  (fn [db]
-    (:players db)))
